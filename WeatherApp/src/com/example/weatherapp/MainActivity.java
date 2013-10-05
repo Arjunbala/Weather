542de7418f717b8bb750183b77ff859972d14d40
Arjunbala <arjunbala0711@gmail.com>
2013-10-05 11:14:08 +0530

    All basic buttons working

--- WeatherApp/src/com/example/weatherapp/MainActivity.java
+++ WeatherApp/src/com/example/weatherapp/MainActivity.java
@@ -3,16 +3,33 @@ package com.example.weatherapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    @Override
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView time = (TextView) findViewById(R.id.time);
         setContentView(R.layout.activity_main);
-    }
+        ImageButton ref = (ImageButton) findViewById(R.id.refresh);
+        ref.setOnClickListener(ref_func);
+        }
+  
+ View.OnClickListener ref_func = new View.OnClickListener() {
+	
+	@Override
+	public void onClick(View v) {
+		TextView time = (TextView)findViewById(R.id.time);
+		TextView city = (TextView)findViewById(R.id.city);
+		TextView status = (TextView)findViewById(R.id.status);
+		TextView temp = (TextView)findViewById(R.id.temperature);
+		time.setText("10:34");
+		city.setText("Chennai");
+		status.setText("Cloudy");
+		temp.setText("30C");
+		}
+};
 
 
     @Override
