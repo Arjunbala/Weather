1d710131efc923913cf023804b9426d1a02d36fa
Arjunbala <arjunbala0711@gmail.com>
2013-10-05 13:07:39 +0530

    Dialogue box to change city created

--- WeatherApp/src/com/example/weatherapp/MainActivity.java
+++ WeatherApp/src/com/example/weatherapp/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.weatherapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	final Context context = this;
+	String change_city = new String();
   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton ref = (ImageButton) findViewById(R.id.refresh);
         ref.setOnClickListener(ref_func);
+        Button change = (Button) findViewById(R.id.change);
+        change.setOnClickListener(change_func);
         }
   
  View.OnClickListener ref_func = new View.OnClickListener() {
@@ -28,6 +37,23 @@ public class MainActivity extends Activity {
 		city.setText("Chennai");
 		status.setText("Cloudy");
 		temp.setText("30C");
+		}
+};
+
+View.OnClickListener change_func = new View.OnClickListener() {
+	
+	@Override
+	public void onClick(View v) {
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+		alertDialogBuilder.setTitle("Change City");
+		alertDialogBuilder.setMessage("Enter the name of the city");
+		final EditText input = new EditText(context);
+		alertDialogBuilder.setView(input);
+		alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				change_city = input.getText().toString();
+			}
+		  });
 		}
 };
 
